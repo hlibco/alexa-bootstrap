541e0d623b4f6c063e775013696d7f162340ee66
Felipe Hlibco <hlibco@gmail.com>
2017-03-15 17:26:59 -0700

    ci(travis): remove duplicated test

--- test/index.js
+++ test/index.js
@@ -1,37 +1,72 @@
 import test from 'ava'
 const App = require('../src/index')
 
-test(`pre()`, t => {
+test(`pre(func)`, t => {
   const app = new App()
   app.pre(() => {})
 
   t.not(app.preFunction, null)
 })
 
-test(`post()`, t => {
+test(`pre()`, t => {
+  const app = new App()
+  app.pre()
+
+  t.is(app.preFunction, null)
+})
+
+test(`post(func)`, t => {
   const app = new App()
   app.post(() => {})
 
   t.not(app.postFunction, null)
 })
 
-test(`launch()`, t => {
+test(`post()`, t => {
+  const app = new App()
+  app.post()
+
+  t.is(app.postFunction, null)
+})
+
+test(`launch(func)`, t => {
   const app = new App()
   app.launch(() => {})
 
   t.not(app.launchFunction, null)
 })
 
-test(`sessionEnded()`, t => {
+test(`launch()`, t => {
+  const app = new App()
+  app.launch()
+
+  t.is(app.launchFunction, null)
+})
+
+test(`sessionEnded(func)`, t => {
   const app = new App()
   app.sessionEnded(() => {})
 
   t.not(app.sessionEndedFunction, null)
 })
 
-test(`intent()`, t => {
+test(`sessionEnded()`, t => {
+  const app = new App()
+  app.sessionEnded()
+
+  t.is(app.sessionEndedFunction, null)
+})
+
+test(`intent(func)`, t => {
   const app = new App()
   app.intent('Confirmation', () => {})
 
   t.truthy(app.intents.Confirmation)
+})
+
+test(`intent()`, t => {
+  const app = new App()
+  app.intent('Confirmation')
+
+  t.deepEqual(app.intents, {})
 })
